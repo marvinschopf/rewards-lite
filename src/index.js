bbd8768d14db2e809cb7ea2dc8b06f7b5bc8c75e
Marvin Schopf <marvin@schopf.biz>
2021-02-06 03:29:29 +0100

    Update file header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * @license
+ * rewards-lite
  * Copyright (c) 2021 Marvin Schopf
  * Copyright (c) 2018-2020 thedevelobear
  * 
@@ -20,6 +20,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ * 
+ * @license MIT
+ * 
  */
 
 import Reward from "./components/Reward";
